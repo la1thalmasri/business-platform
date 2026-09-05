98dcc4eda0f942fb47790cbaebb1857972c9e156
laith <laithalmasri079@gmail.com>
2026-09-05 06:13:40 +0300

    Update products.js

--- javascript/inventory-app/src/products.js
+++ javascript/inventory-app/src/products.js
@@ -1,84 +1,93 @@
+//Task5
+const products = [
 
-//task 4
+    {
+        id: 0,
+        name: "milk",
+        price: 0.5,
+        quantity: 5,
+        expiryDate: "2026-10-01",
+        minimumStock: 10,
 
+    },
 
-const products =[
-  {
-     id:0,
-    name : "Milk",
-    price :0.75 +" JOD",
-    quantity : 35,
-    expiryDate :"oct/08/2026",
-    minimumStock:10,
-    active : false,
-  },
+    {
+        id: 1,
+        name: "Chocolate Galaxy",
+        price: 1,
+        quantity: 30,
+        expiryDate: "2026-10-02",
+        minimumStock: 0,
 
-{
-  id:1,
-    name : "Chocolate Galaxy",
-    price :1 +" JOD",
-    quantity :45 ,
-    expiryDate :"oct/15/2026",
-    minimumStock:20,
-    active : true,
-},
-{
-  id:2,
-    name : "Chocolate Mars",
-    price : 1.5 +" JOD",
-    quantity : 50,
-    expiryDate :"oct/20/2026",
-    minimumStock:30,
-    active : false,
-},
+    },
 
+    {
+        id: 2,
+        name: "rice",
+        price: 6,
+        quantity: 40,
+        expiryDate: "2026-07-03",
+        minimumStock: 30,
 
-{
-      id:3,
-    name : "braed",
-    price : 2 +" JOD",
-    quantity : 70,
-    expiryDate :"oct/25/2026",
-    minimumStock:40,
-    active : true,
-},
+    },
 
+    {
+        id: 3,
+        name: "braed",
+        price: 2,
+        quantity: 50,
+        expiryDate: "2026-10-04",
+        minimumStock: 40,
 
-{
-      id:4,
-    name : "Chocolate",
-    price : 1.5 +" JOD",
-    quantity : 80,
-    expiryDate :"oct/28/2026",
-    minimumStock:50,
-    active : false,
-},
+    },
 
-{
-    id:5,
-    name : "Chocolate laith",
-    price : 2 +" JOD",
-    quantity : 90,
-    expiryDate :"oct/11/2026",
-    minimumStock:60,
-    active : true,
-}
+    {
+        id: 4,
+        name: "coffee",
+        price: 7,
+        quantity: 50,
+        expiryDate: "2026-10-05",
+        minimumStock: 50,
+
+    },
+
 ];
 
 
- const product = products.find(function(gitProductId){
- return gitProductId.id === 3
- });
-console.log(product);
+function isLowStock(product) {
+    return product.quantity <= product.minimumStock;
+}
+
+console.log(isLowStock(products[0]));
 
 
-const quantity = products.filter(function (quantityy1){
-return quantityy1.quantity >10;
-});
-console.log(quantity);
+
+function isOutOfStock(product) {
+    return product.quantity === 0;
+}
+
+console.log(isOutOfStock(products[1]));
 
 
-products.filter(function(minimumStockk2){
-return products.quantity <= products.minimumStock;
-});
-console.log(products);
+
+function isExpired(product) {
+    return new Date(product.expiryDate) < new Date();
+}
+
+console.log(isExpired(products[2]));
+
+
+
+function calculateInventoryValue(product) {
+    return product.price * product.quantity;
+}
+
+console.log(calculateInventoryValue(products[4]));
+
+
+
+function decreaseStock(product, quantity) {
+    return Math.max(0, product.quantity - quantity);
+}
+
+console.log(decreaseStock(products[4], 40));
