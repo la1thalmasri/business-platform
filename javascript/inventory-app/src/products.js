19765812c61829003a6bb281cbdde0f91e85d698
laith <laithalmasri079@gmail.com>
2026-09-05 06:06:14 +0300

    Update products.js

--- javascript/inventory-app/src/products.js
+++ javascript/inventory-app/src/products.js
@@ -1,46 +1,79 @@
-// Task 2
+//task 3
 
+const products =[
+{
+    id:0,
+    name : "Milk",
+    price : 1.5,
+    quantity : 20,
+    expiryDate :"oct/08/2026",
+    minimumStock:5,
+    active : false,
+},
+{
+  id:1,
+    name : "Chocolate Galaxy",
+    price :0.5,
+    quantity :30 ,
+    expiryDate :"oct/15/2026",
+    minimumStock:8,
+    active : true,
+},
+{
+  id:2,
+    name : "Chocolate Mars",
+    price : 0.5,
+    quantity : 40,
+    expiryDate :"oct/20/2026",
+    minimumStock:10,
+    active : false,
+},
+{
+      id:3,
+    name : "braed",
+    price : 0.2,
+    quantity : 50,
+    expiryDate :"oct/25/2026",
+    minimumStock:15,
+    active : true,
+},
+{
+      id:4,
+    name : "Chocolate",
+    price : 1,
+    quantity : 60,
+    expiryDate :"oct/28/2026",
+    minimumStock:20,
+    active : false,
+},
 
-var storename = "Sober Market Laith";       // اسم المتجر                          : String  
-var storeowner ="laith";                  // صاحبالمحل                           : String
-var productname ="Chocolate Galaxy";     // اسم المنتج                         : String
-var productprice = 1 ;                   //سعر المنتخ                          : Number
-var productquantity = 100;                  // كمية المنتج                    : Number
-var productexpirydate ="Oct/30/2026";              // تاريخ انتهاء صلاحي          : Date / String
-var productactivestatus = false;            // حاله تفعيل المنتج   اه او لا : boolean
-var customername ="Osama";                 // اسم الزبون                        : string
-var customerphone ="00962791811112";               // رقم تلفون الزبون                  : String
-var inventoryvalue = productprice * productquantity;  // هاي لحساب المخزن و الفكرة في ال * بس
+{
+    id:5,
+    name : "Chocolate laith",
+    price : 6,
+    quantity : 70,
+    expiryDate :"oct/11/2026",
+    minimumStock:25,
+    active : true,
+}
+];
 
-console.log(storename);
-console.log(storeowner);
-console.log(productname);
-console.log(productprice);
-console.log(productquantity);
-console.log(productexpirydate);
-console.log(productactivestatus);
-console.log(customername);
-console.log(customerphone);
-console.log(inventoryvalue);
+console.log(products);
 
-var storename ="Sober Market almasri";
-var storeowner ="ayman";
-var productname ="Chocolate mars";
-var productprice =1.5;
-var productquantity = 90;
-var productexpirydate ="Oct/30/2026";
-var productactivestatus =false;
-var customername ="mohamd";
-var customerphone ="00962123456"; 
-var inventoryvalue = productprice * productquantity;
+const Milk = products.find(product=>product.name=="Milk");
 
-console.log(storename);
-console.log(storeowner);
-console.log(productname);
-console.log(productprice);
-console.log(productquantity);
-console.log(productexpirydate);
-console.log(productactivestatus);
-console.log(customername);
-console.log(customerphone);
-console.log(inventoryvalue);
+Milk.price =2.5;
+Milk.quantity=10;
+Milk.active=false;
+console.log(Milk);
+
+products[3].price =50;
+products[3].quantity=25;
+products[3].active=false;
+console.log(products [3]);
+
+const product = products.find(product=> product.id ==4);
+product.price =99;
+product.quantity=40;
+product.active=true;
+console.log(product);
