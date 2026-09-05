2909fa66b53213b000ecc9a2adaf3dd1695e9663
laith <laithalmasri079@gmail.com>
2026-09-05 05:59:37 +0300

    Add products tasks

---
+++ javascript/inventory-app/src/products.js
@@ -0,0 +1,46 @@
+// Task 2
+
+
+var storename = "Sober Market Laith";       // اسم المتجر                          : String  
+var storeowner ="laith";                  // صاحبالمحل                           : String
+var productname ="Chocolate Galaxy";     // اسم المنتج                         : String
+var productprice = 1 ;                   //سعر المنتخ                          : Number
+var productquantity = 100;                  // كمية المنتج                    : Number
+var productexpirydate ="Oct/30/2026";              // تاريخ انتهاء صلاحي          : Date / String
+var productactivestatus = false;            // حاله تفعيل المنتج   اه او لا : boolean
+var customername ="Osama";                 // اسم الزبون                        : string
+var customerphone ="00962791811112";               // رقم تلفون الزبون                  : String
+var inventoryvalue = productprice * productquantity;  // هاي لحساب المخزن و الفكرة في ال * بس
+
+console.log(storename);
+console.log(storeowner);
+console.log(productname);
+console.log(productprice);
+console.log(productquantity);
+console.log(productexpirydate);
+console.log(productactivestatus);
+console.log(customername);
+console.log(customerphone);
+console.log(inventoryvalue);
+
+var storename ="Sober Market almasri";
+var storeowner ="ayman";
+var productname ="Chocolate mars";
+var productprice =1.5;
+var productquantity = 90;
+var productexpirydate ="Oct/30/2026";
+var productactivestatus =false;
+var customername ="mohamd";
+var customerphone ="00962123456"; 
+var inventoryvalue = productprice * productquantity;
+
+console.log(storename);
+console.log(storeowner);
+console.log(productname);
+console.log(productprice);
+console.log(productquantity);
+console.log(productexpirydate);
+console.log(productactivestatus);
+console.log(customername);
+console.log(customerphone);
+console.log(inventoryvalue);
